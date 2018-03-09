51c1feda0ae18d87dbcf3af7f36687443d15fc65
idubnori <i.dub.nori@gmail.com>
2018-03-10 02:36:49 +0900

    Changed to use DefaultMessageSouce or DefaultMessagePublisher.

--- Obvs.NoneEndpoint/NoneEndpoint.cs
+++ Obvs.NoneEndpoint/NoneEndpoint.cs
@@ -1,24 +1,16 @@
-﻿using System;
-using System.Reactive.Linq;
-using System.Threading.Tasks;
-using Obvs.Types;
+﻿using Obvs.Types;
 
 namespace Obvs.NoneEndpoint
 {
-    public class NoneEndpoint : IServiceEndpoint
+    public class NoneEndpoint : ServiceEndpoint
     {
-        public bool CanHandle(IMessage message) => false;
-
-        public string Name => nameof(NoneEndpoint);
-        public IObservable<IRequest> Requests { get; } = Observable.Empty<IRequest>();
-
-        public IObservable<ICommand> Commands { get; } = Observable.Empty<ICommand>();
-
-        public Task PublishAsync(IEvent ev) => Task.FromResult(0);
-
-        public Task ReplyAsync(IRequest request, IResponse response) => Task.FromResult(0);
-
-        public void Dispose()
+        public NoneEndpoint()
+            : base(
+                new DefaultMessageSource<IRequest>(),
+                new DefaultMessageSource<ICommand>(),
+                new DefaultMessagePublisher<IEvent>(),
+                new DefaultMessagePublisher<IResponse>(),
+                typeof(NoneEndpoint))
         {
         }
     }
